52d3c5f580c787baee33ae4fb077396fca5528f1
Shahbal123 <netshahbal@gmail.com>
2026-04-10 16:44:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function showThankYou()
 {document.getElementById("message").textContent = "Thanks for clicking the button!🎉";
 }
+
+function toggleDarkMode(){document.body.clasList.toggle("dark-mode";}
